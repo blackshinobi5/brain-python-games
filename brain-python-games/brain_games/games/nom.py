ebfa613403a23f0a3ea6988681dc428822c38d3c
blackshinobi5 <107775615+blackshinobi5@users.noreply.github.com>
2025-04-22 21:05:52 +0300

    Update nom.py

--- brain-python-games/brain_games/games/nom.py
+++ brain-python-games/brain_games/games/nom.py
@@ -1,27 +1,11 @@
 import math
 import random
 
+def lcm(a, b, c):
+    return math.lcm(a, b, c)
 
-def find_lcm(a, b, c):
-    lcm_ab = (a * b) // math.gcd(a, b)
-    lcm_abc = (lcm_ab * c) // math.gcd(lcm_ab, c)
-    return lcm_abc
-
-
-def play():
-    print("Найди наименьшее общее кратное для чисел ниже.")
-    for _ in range(3):
-        a = random.randint(1, 10)
-        b = random.randint(1, 10)
-        c = random.randint(1, 10)
-        lcm = find_lcm(a, b, c)
-
-        print(f"{a} {b} {c}")
-        user_answer = int(input("Твой ответ: "))
-
-        if user_answer != lcm:
-            print(f"Ты умер(ла)! {user_answer} - это неправильный ответ :<."
-                  f" Правильный ответ был - {lcm}.")
-            return False
-        print("Молодец! Ты прошел уровень!")
-    return True
+def get_lcm_game_data():
+    numbers = [random.randint(1, 100) for _ in range(3)]
+    question = " ".join(map(str, numbers))
+    correct_answer = str(lcm(*numbers))
+    return question, correct_answer
